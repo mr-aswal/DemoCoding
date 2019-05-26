5b67cabe3f1cd0e4bec77fa8b08c011274d8ea5d
mr-aswal <aaswal346@gmail.com>
2019-05-27 03:25:31 +0530

    this is first version

---
+++ src/main/java/com/dummy/base/DummyBase.java
@@ -0,0 +1,16 @@
+package com.dummy.base;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class DummyBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+	public void testbase()
+	{
+		
+		
+	}
+}
---
+++ src/test/java/com/dummy/maven/Login.java
@@ -0,0 +1,15 @@
+package com.dummy.maven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	public void lgn()
+	{
+		System.out.println("hello world");
+		Assert.assertEquals("Abc", "Abc");
+	}
+
+}
