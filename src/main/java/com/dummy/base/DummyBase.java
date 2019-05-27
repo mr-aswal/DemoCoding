a2017d197c2787c316b2c9de08c2bb84146f9b96
mr-aswal <aaswal346@gmail.com>
2019-05-27 09:09:56 +0530

    Update DummyBase.java

--- src/main/java/com/dummy/base/DummyBase.java
+++ src/main/java/com/dummy/base/DummyBase.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 
 public class DummyBase {
-
+	//this is test
 	public static WebDriver driver;
 	public static Properties prop;
 	public void testbase()
