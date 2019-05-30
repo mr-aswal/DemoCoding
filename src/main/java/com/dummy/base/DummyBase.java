0e2903c80b841ae7336ef32a193bd8fa9b33aae3
mr-aswal <aaswal346@gmail.com>
2019-05-30 08:38:49 +0530

    initialized webdriver

--- src/main/java/com/dummy/base/DummyBase.java
+++ src/main/java/com/dummy/base/DummyBase.java
@@ -1,16 +1,42 @@
 package com.dummy.base;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class DummyBase {
 	//this is test
 	public static WebDriver driver;
 	public static Properties prop;
-	public void testbase()
+	
+	
+	public  DummyBase()
 	{
 		//this is test only
+		try {
 		
+			FileInputStream in= new FileInputStream("F:\\my workspace\\com.dummy.maven\\src\\main\\java\\com\\dummy\\config\\config.properties");
+			prop.load(in);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+			
+	}
+	
+	public void initialization()
+	{
+		System.setProperty(prop.getProperty("systemprop1"), prop.getProperty("systemprop2"));
+		
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+			
+			driver.manage().window().maximize();
+		}
 	}
 }
